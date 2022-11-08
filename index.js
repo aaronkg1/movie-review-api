ec066b2d9bcfec84d5bf72fc090557e8bcf6e977
aaronkg1 <aaronkg1@gmail.com>
2022-11-08 16:02:59 +0000

    Change to package.json

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ import dotenv from "dotenv";
 const app = express();
 dotenv.config();
 const port = process.env.PORT || 4000;
-app.set("port", port);
 const startServer = async () => {
 	try {
 		//Attempt mongodb connection
